921de11d8e3eea597c2cb5c3a59e8efd6fcc6458
Antonio Nikishaev <me@lelf.lu>
2020-05-26 23:33:02 +0400

    pp Adv'd

--- K.hs
+++ K.hs
@@ -56,7 +56,7 @@ siz(Ls a)=R∘A∘N∘η∘len$a;siz _=Er"rank"
 class PP α where pp::α->S
 instance PP E   where{pp(A l)=pp l;pp(Var v)=v;pp(Ap a x)=fmt"(%s)[%s]"(pp a)$semi$pp<$>x;pp(Ass v e)=fmt"%s:%s"v$pp e;
                       pp(Fun f)=pp f;pp(Ls s)=pr∘semi$pp<$>s;pp Nil="";pp(Seq x)=semi$pp<$>x}
-instance PP Fun where pp(Op o)=pp o;pp(Lam a b)=fmt"{[%s]%s}"(semi a)(pp b);pp Adv'd{}=error"pp:f:adv"
+instance PP Fun where pp(Op o)=pp o;pp(Lam a b)=fmt"{[%s]%s}"(semi a)(pp b);pp(Adv'd a x)=(pp x⊗)∘π∘("/\\':"!!)∘fromEnum$a
 instance PP L   where pp(N(J x))=sw x⊗"j";pp(N(O x))=sw x⊗"o";pp(C c)=fmt"\"%c\""c;pp(Sy s)='`':s
 instance PP Op  where pp(:--)="_";pp(:..)=",";pp o=π∘(!!2)∘sw$o
 
