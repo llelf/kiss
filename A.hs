beba5a7f75f192a5454c1fcebcca6fc60501b7ea
Antonio Nikishaev <me@lelf.lu>
2020-06-19 15:04:37 +0400

    floats

--- A.hs
+++ A.hs
@@ -1,13 +1,13 @@
 {-#language DeriveGeneric#-}
 module A where
 import Data.Int;import GHC.Generics(Generic)
-type I=Int32;type J=Int64;type O=Integer;type S=String;type C=Char;type Sy=S;type V=S;type B=Bool
+type I=Int32;type J=Int64;type O=Integer;type F=Double;type S=String;type C=Char;type Sy=S;type V=S;type B=Bool
 
 data E=A L|Ls[E]|{-|Dic[E][E]-}Fun Fun|Var V|Ap E[E]|Ass E E|Cond[E]|Seq[E]|Nil|Com[E] deriving(Eq,Ord,Show,Generic)
 {- ==> E α=A..|L[α]|D[α][α] -}
 
 data L=N N|C C|Sy Sy deriving(Eq,Ord,Show);   data Fun=Op Op|Lam[V]E|Adv'd Adv E deriving(Eq,Ord,Show)
-data N=J J|O O       deriving(Eq,Ord,Show);   data Adv=Fold|Scan|Each            deriving(Eq,Ord,Enum,Show,Bounded)
+data N=J J|O O|F F   deriving(Eq,Ord,Show);   data Adv=Fold|Scan|Each            deriving(Eq,Ord,Enum,Show,Bounded)
  
 data Op=(:~)|(:!)|(:@)|(:#)|(:$)|(:%)|(:^)|(:&)|(:*)|(:-)|(:=)|(:--)|(:+)|(:..)|(:.)|(:<)|(:>)|(:?)|(:|)
  deriving(Eq,Ord,Enum,Read,Show,Bounded)
--- K.hs
+++ K.hs
@@ -5,7 +5,7 @@ import A; import Data.Function;import Text.Printf(printf);import Data.Functor.Id
 import P; import Data.Int;import Data.List;import Control.Monad.State;import Test.QuickCheck hiding(discard,Fun)
 
 type ΓΓ=[(V,E)];type Γ=[(V,(Sc,E))];type Env=Γ;type Σ=Γ;type(+)=Either;data Sc=Gl|Lo deriving(Eq,Show)
-type M=StateT Σ((+)S); pattern R x=Right x; pattern Er x=Left x; pattern T=True::Bool; pattern F=False
+type M=StateT Σ((+)S); pattern R x=Right x; pattern Er x=Left x; pattern T=True::Bool; pattern NO=False
 
 (∘)=(.);(⊗)=(<>);π=pure;φ=lift;(??)=flip;(<∘>)=fmap∘fmap;infixl 4<∘>;len=length;sw=show;trv=traverse;seqA=sequenceA
 rev=reverse;fmt=printf;(∅)=mempty;η=fromIntegral;er=φ∘Er;nyi=er∘("nyi."⊗);wow=φ∘R;such=wow; meh=error"xkcd.com/292"
@@ -23,7 +23,7 @@ ev(Cond[c,a,b])=do{i<-ev c;ev$if ist i then a else b};ev Cond{}=nyi"¬3.cond";ev
 ev(Ass(Var v)e)=do{(s,_)<-getV' v;ev e>>=setV s v};ev(Ass _ _)=nyi"ass.cmlx";ev x=wow x
 
 ty::E->Ty;ty(A(N(O _)))=Ta To;ty(A(N(J _)))=Ta Tj;ty(Ls x)|[Ta t]<-nub∘sort$ty<$>x=Tl t|T=TL;ty(Fun _)=TF;ty _=NT
-ist::E->B;ist(A(N x))=x/=0;ist(Ls[])=F;ist Ls{}=T
+ist::E->B;ist(A(N x))=x/=0;ist(Ls[])=NO;ist Ls{}=T
 
 eap::E->[E]->M E; eap(Fun(Op o))a=eop o a;eap(Fun(Lam v e))a=elam v e a
 eap(Fun(Adv'd Fold e))[a]=efld e a;eap Fun{} _=nyi"fun.smth";
@@ -63,21 +63,23 @@ siz(Ls a)=R∘A∘N∘η∘len$a;siz _=Er"rank";gup(Ls a)=R∘Ls$A∘N∘η<$>so
 
 
 class PP α where pp::α->S
-instance PP Fun where pp(Op o)=pp o;pp(Lam a b)=fmt"{[%s]%s}"(semi a)(pp b);pp(Adv'd a x)=(ppr x⊗)∘π∘("/\\'"!!)∘fromEnum$a
-instance PP L   where pp(N(J x))=sw x;pp(N(O x))=sw x;pp(C c)=fmt"\"%c\""c;pp(Sy s)='`':s
+instance PP Fun where pp(Op o)=pp o;pp(Lam a b)=fmt"{[%s]%s}"(semi a)(pp b);pp(Adv'd a x)=(ppoa x⊗)∘π∘("/\\'"!!)∘fromEnum$a
+instance PP L   where pp(N(J x))=sw x;pp(N(O x))=sw x;pp(N(F x))=sw x;pp(C c)=fmt"\"%c\""c;pp(Sy s)='`':s
 instance PP Op  where pp(:--)="_";pp(:..)=",";pp o=π∘(!!2)∘sw$o
 instance PP E   where{pp(A l)=pp l;  pp(Ls[x])=',':prpf x; pp x@(Ls s)|TL<-ty x=pr∘semi$pp<$>s|T=ict" "$pp<$>s;pp Nil=(∅);
                       pp(Fun f)=pp f;pp(Var v)=v;pp(Ass x e)|Var v<-x=v⊗":"⊗pp e|T=ppr x⊗":"⊗pp e;pp(Seq x)=semi$pp<$>x;
                       pp(Ap(Fun(Op o))[x,y])=let p A{}=pp x;p Var{}=pp x;p(Ls[x])=pr(',':prpf x);p Ls{}=pp x;p x=ppr x
                        in prc(o==(:.))(p x)⊗pp o⊗prc(o`elem`[(:-),(:.)])(prpf y);
-                      pp(Ap(Fun(Op o))[x])=pp o⊗spmd o⊗prpf x; pp(Ap a x)=fmt"(%s)[%s]"(pp a)∘semi$pp<$>x}
+                      pp(Ap(Fun(Op o))[x])=pp o⊗spmd o⊗prpf x; pp(Ap a x)=psap a x}
 
+ppoa(Fun(Op o))=pp o;ppoa(A a)=pp a;ppoa x=ppr x
+psap f x=fmt"%s[%s]"(z f)∘semi$pp<$>x where z Fun{}=pp f;z _=ppr f
 ict=intercalate;prpf x@Fun{}=pr∘pp$x; prpf x=pp x; spmd o=[' '|o`elem`[(:-),(:.)]]; prc c|c=pr|T=id
 semi=ict";";pr=fmt"(%s)";ppr=pr∘pp;esc::S->S;esc(c:s)|c`elem`"\"\n"='\\':c:esc s|T=c:esc s;esc _=[]
 arb::_=>Gen a;arb=arbitrary;frq=frequency;elms=elements[minBound..];smol q=sized$(resize??q)∘(`div`3)
 ilist=Ls<∘>(<∘>)A∘smol∘listOf$arb @L;avar=Var<$>elements["x","y","foo"]
 
-instance Arbitrary L   where arbitrary=N∘O<$>arb; shrink=π[N 0]
+instance Arbitrary L   where arbitrary=N<$>frq[(2,O<$>arb),(1,F<$>arb)]; shrink=π[N 0]
 instance Arbitrary E   where arbitrary=frq[(4,A<$>arb),(2,ilist),(1,Ls<$>smol arb),(1,avar),(1,Fun<$>arb),
                               (2,Ap<$>frq[(5,Fun<$>arb),(1,arb)]
                                    <*>frq[(3,π<$>smol arb),(3,(:)<$>smol arb<*>smol arb),(1,smol arb)]),
